09f560d832dc702ed65cad75f9f8087ebcb043c8
Nils-Broder <nils-broder.thomsen@stud.hs-flensburg.de>
2026-01-13 17:27:22 +0100

    Woche 5 Progress

--- testBashScript/apiTest.sh
+++ testBashScript/apiTest.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo "== Get all entries =="
-curl -X GET http://localhost:8080/api/users/1/entries
-echo -e "\n----------------------\n"
+#echo "== Get all entries =="
+#curl -X GET http://localhost:8080/api/users/1/entries
+#echo -e "\n----------------------\n"
 
 echo "== Create entry =="
 curl -X POST http://localhost:8080/api/users/1/entries \
@@ -14,27 +14,27 @@ curl -X POST http://localhost:8080/api/users/1/entries \
   }'
 echo -e "\n----------------------\n"
 
-echo "== Create entry without mood =="
-curl -X POST http://localhost:8080/api/users/1/entries \
-  -H "Content-Type: application/json" \
-  -d '{
-    "title": "Ohne Stimmung",
-    "content": "Kein MoodRating gesetzt"
-  }'
-echo -e "\n----------------------\n"
-
-echo "== Update entry =="
-curl -X PUT http://localhost:8080/api/entries/1 \
-  -H "Content-Type: application/json" \
-  -d '{
-    "title": "Aktualisierter Titel",
-    "content": "Aktualisierter Inhalt",
-    "moodRating": 7
-  }'
-echo -e "\n----------------------\n"
-
-echo "== Try delete that entry =="
-curl -X DELETE http://localhost:8080/api/entries/1
-echo -e "\n----------------------\n"
+#echo "== Create entry without mood =="
+#curl -X POST http://localhost:8080/api/users/1/entries \
+#  -H "Content-Type: application/json" \
+#  -d '{
+#    "title": "Ohne Stimmung",
+#    "content": "Kein MoodRating gesetzt"
+#  }'
+#echo -e "\n----------------------\n"
+#
+#echo "== Update entry =="
+#curl -X PUT http://localhost:8080/api/entries/1 \
+#  -H "Content-Type: application/json" \
+#  -d '{
+#    "title": "Aktualisierter Titel",
+#    "content": "Aktualisierter Inhalt",
+#    "moodRating": 7
+#  }'
+#echo -e "\n----------------------\n"
+#
+#echo "== Try delete that entry =="
+#curl -X DELETE http://localhost:8080/api/entries/1
+#echo -e "\n----------------------\n"
 
 read -p "Press enter to exit"
