807173035e7889eb7ec27a01f51e97176b318aa6
NilsBroderThomsen <91457410+NilsBroderThomsen@users.noreply.github.com>
2026-01-23 19:48:08 +0100

    Implement js client entry loading

--- js-client/build.gradle.kts
+++ js-client/build.gradle.kts
@@ -14,6 +14,8 @@ kotlin {
         jsMain {
             dependencies {
                 implementation(project(":client"))
+                implementation(project(":common"))
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
                 implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.11.0")
             }
         }
