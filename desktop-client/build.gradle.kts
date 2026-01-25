cd2313a076efdf5a2066f71dbf70c39bc0aefad1
NilsBroderThomsen <91457410+NilsBroderThomsen@users.noreply.github.com>
2026-01-25 02:35:36 +0100

    Add navigation and serialization deps

--- desktop-client/build.gradle.kts
+++ desktop-client/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
 }
@@ -12,8 +13,10 @@ dependencies {
     implementation(project(":client"))
     implementation(project(":common"))
     implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.serialization.core)
     implementation(compose.desktop.currentOs)
     implementation(libs.compose.material3)
+    implementation(libs.androidx.navigation.compose)
 }
 
 compose.desktop {
