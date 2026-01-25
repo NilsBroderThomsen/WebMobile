5557e4f5951d26404fb2a541a39d0d1d73cbe31f
Nils-Broder <nils-broder.thomsen@stud.hs-flensburg.de>
2026-01-25 01:11:08 +0100

    basic desktop-client progress

--- desktop-client/src/main/kotlin/Main.kt
+++ desktop-client/src/main/kotlin/Main.kt
@@ -5,6 +5,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
@@ -24,9 +28,11 @@ fun App() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Text("Hello World")
-            Button(onClick = {}) {
-                Text("Click Me")
+            var count by remember { mutableStateOf(0) }
+
+            Text("Count: $count")
+            Button(onClick = { count++ }) {
+                Text("+1")
             }
         }
     }
