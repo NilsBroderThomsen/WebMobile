5a8ee93c7b1989c059dbda818cf2e97b95750b8e
Nils-Broder <nils-broder.thomsen@stud.hs-flensburg.de>
2026-01-25 02:22:19 +0100

    basic desktop-client progress

--- desktop-client/src/main/kotlin/Main.kt
+++ desktop-client/src/main/kotlin/Main.kt
@@ -1,11 +1,29 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
+import api.MoodTrackerClient
+import dto.EntryDto
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "Hello Compose") {
+    Window(onCloseRequest = ::exitApplication, title = "Mood Tracker") {
         App()
     }
 }
@@ -19,18 +37,10 @@ fun App() {
             startDestination = HomePage
         ) {
             composable<HomePage> {
-                StudentsList(
-                    onNavigateToSettings = {
-                        navController.navigate(SettingsPage)
-                    }
-                )
+                HomeView()
             }
-            composable<SettingsPage> {
-                SettingsView(
-                    onNavigateBack = {
-                        navController.navigate(HomePage)
-                    }
-                )
+            composable<EntriesPage> {
+                EntryList(1L)
             }
         }
     }
@@ -39,5 +49,30 @@ fun App() {
 @Serializable
 object HomePage
 
+@Composable
+fun HomeView() {
+    Button(onClick = { /*TODO*/ }) {
+        Text("My Entries")
+    }
+}
+
 @Serializable
-object SettingsPage
+object EntriesPage
+
+@Composable
+fun EntryList(userId: Long) {
+    val baseUrl = "http://localhost:8080"
+    var entries by remember { mutableStateOf<List<EntryDto>>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        val client = MoodTrackerClient(baseUrl)
+        entries = client.getEntries(userId)
+    }
+
+    Column {
+        entries.forEach { entry ->
+            Text("Entry ID: ${entry.id}, Title: ${entry.title}, Content: ${entry.content}, Mood: ${entry.moodRating}")
+        }
+    }
+}
+
