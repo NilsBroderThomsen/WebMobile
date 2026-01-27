eae2f7028e61b7b4933a608954fb89087a2d9630
NilsBroderThomsen <91457410+NilsBroderThomsen@users.noreply.github.com>
2026-01-28 00:39:47 +0100

    Add Android library plugin to shared client

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
 }
 
 kotlin {
@@ -29,5 +30,14 @@ kotlin {
                 implementation(libs.ktor.client.okhttp)
             }
         }
+    }
+}
+
+android {
+    namespace = "de.hsflensburg.moodtracker.client"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
     }
 }
