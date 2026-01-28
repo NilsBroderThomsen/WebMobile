92a7ca72d877591fefd8ea9b1d719e9cb170555c
NilsBroderThomsen <91457410+NilsBroderThomsen@users.noreply.github.com>
2026-01-28 19:31:41 +0100

    Limit logback dependency to JVM targets

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -17,12 +17,12 @@ kotlin {
                 implementation(libs.ktor.client.core)
                 implementation(libs.ktor.client.content.negotiation)
                 implementation(libs.ktor.serialization.kotlinx.json)
-                implementation(libs.logback.classic)
             }
         }
         jvmMain {
             dependencies {
                 implementation(libs.ktor.client.cio)
+                implementation(libs.logback.classic)
             }
         }
         androidMain {
