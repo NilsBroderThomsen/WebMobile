85217dcf28d03a7dd4c715fba4a35d6454b1bf82
Nils-Broder <nils-broder.thomsen@stud.hs-flensburg.de>
2026-01-30 15:22:30 +0100

    remove default test entries

--- backend/src/main/kotlin/database/DatabaseFactory.kt
+++ backend/src/main/kotlin/database/DatabaseFactory.kt
@@ -1,18 +1,10 @@
 package database
 
-import database.dao.EntryDAO
-import database.dao.UserDAO
 import database.tables.EntriesTable
 import database.tables.UsersTable
-import extension.toJavaInstant
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.todayIn
-import kotlinx.datetime.toJavaLocalDate
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
-import security.PasswordHasher
-import kotlin.time.Clock
 
 object DatabaseFactory {
     fun init() {
@@ -26,24 +18,6 @@ object DatabaseFactory {
                 UsersTable,
                 EntriesTable
             )
-
-            val defaultUser = UserDAO.findById(1L) ?: UserDAO.new(1L) {
-                username = "default-user"
-                email = "default@example.com"
-                passwordHash = PasswordHasher.hash("test")
-                registrationDate = Clock.System.todayIn(TimeZone.currentSystemDefault()).toJavaLocalDate()
-                isActive = true
-            }
-            if (false) {
-                EntryDAO.new {
-                    user = defaultUser
-                    title = "Mein erster Eintrag"
-                    content = "Automatisch beim Start angelegt."
-                    moodRating = 4
-                    createdAt = Clock.System.now().toJavaInstant()
-                    updatedAt = null
-                }
-            }
         }
     }
 }
